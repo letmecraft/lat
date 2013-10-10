9a676caf0bdceba3007b4ae853b4a02d2de436cf
Thomas <thomas.pob@gmail.com>
2013-10-10 00:34:15 -0400

    Changed voyages en partance dans le backend

--- apps/backend/modules/default/templates/_menu.php
+++ apps/backend/modules/default/templates/_menu.php
@@ -1,6 +1,6 @@
 <ul id="menu">
     <li><a href="<?php echo url_for("@story") ?>">Carnets de voyage</a></li>
-    <li><a href="<?php echo url_for("@journey") ?>">Voyages en partance</a></li>
+    <li><a href="<?php echo url_for("@journey") ?>">Séjours encadrés</a></li>
     <li><a href="<?php echo url_for("@info") ?>">News</a></li>
     <li><a href="<?php echo url_for("@story_place") ?>">Lieux</a></li>
     <li>-</li>
