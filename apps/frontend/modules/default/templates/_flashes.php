e3e21f3487a5e18422116873e56b670d4fd8e275
Thomas <thomas.pob@gmail.com>
2013-10-10 01:42:38 -0400

    Added newsletter 2

---
+++ apps/frontend/modules/default/templates/_flashes.php
@@ -0,0 +1,7 @@
+<?php if($sf_user->hasFlash('notice') != false): ?>
+<div class="flash notice"><?php echo $sf_user->getFlash('notice') ?></div>
+<?php endif ?>
+
+<?php if($sf_user->hasFlash('error') != false): ?>
+<div class="flash error"><?php echo $sf_user->getFlash('error') ?></div>
+<?php endif ?>
